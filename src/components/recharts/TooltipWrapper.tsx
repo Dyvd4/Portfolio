bfbc382cc4ec579bec4b614f899178c916479e43
Dyvd4 <david.kimmich@gmx.net>
2023-01-26 17:37:15 +0100

    added border to tooltip

--- src/components/recharts/TooltipWrapper.tsx
+++ src/components/recharts/TooltipWrapper.tsx
@@ -5,7 +5,8 @@ type TooltipWrapperProps = PropsWithChildren & ComponentPropsWithRef<"div">
 function TooltipWrapper({ children, className, ...props }: TooltipWrapperProps) {
 	return (
 		<div className={`p-2 bg-gray-50 dark:bg-gray-900
-						rounded-md text-sm
+						border-black dark:border-white
+						rounded-md text-sm border
 						text-start ${className}`}
 			{...props}>
 			{children}
