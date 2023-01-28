85c44badaca929212fcc4065a2dc7254f14f1c45
Dyvd4 <david.kimmich@gmx.net>
2023-01-28 23:46:04 +0100

    changed url for cron job

--- src/pages/api/projects/import.ts
+++ src/pages/api/projects/import.ts
@@ -6,14 +6,14 @@ const { IMPORT_PROJECTS_CRON_ACCESS_TOKEN } = config;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-    const IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST = req.headers.authorization?.split("Bearer ")[1];
+	const IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST = req.headers.authorization?.split("Bearer ")[1];
 
-    if (IMPORT_PROJECTS_CRON_ACCESS_TOKEN !== IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST) {
-        res.status(401).json("Not authenticated to trigger trigger route")
-    }
-    else {
-        await ProjectService.fetchProjects();
-        res.json("Successfully imported repos from GitHub");
-    }
+	if (IMPORT_PROJECTS_CRON_ACCESS_TOKEN !== IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST) {
+		res.status(401).json("Not authenticated to trigger import route")
+	}
+	else {
+		await ProjectService.fetchProjects();
+		res.json("Successfully imported repos from GitHub");
+	}
 
 }
