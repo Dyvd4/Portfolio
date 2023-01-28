c2dd76aaa84fc1f7ac644abaff1c1638ca5f3c68
Dyvd4 <david.kimmich@gmx.net>
2023-01-28 15:25:14 +0100

    changed background to white

--- src/components/recharts/TooltipWrapper.tsx
+++ src/components/recharts/TooltipWrapper.tsx
@@ -4,7 +4,7 @@ type TooltipWrapperProps = PropsWithChildren & ComponentPropsWithRef<"div">
 
 function TooltipWrapper({ children, className, ...props }: TooltipWrapperProps) {
 	return (
-		<div className={`p-2 bg-gray-50 dark:bg-gray-900
+		<div className={`p-2 bg-white dark:bg-gray-900
 						border-black dark:border-white
 						rounded-md text-sm border
 						text-start ${className}`}
