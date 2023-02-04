a8b8370e758f6c4733f72e2e9d51208d414ac98d
Dyvd4 <david.kimmich@gmx.net>
2023-02-04 18:49:04 +0100

    increased performance if import
    -- 10s --> 3s
    -- execute imports parallelly instead of sequentially

--- src/backend/services/ProjectService.ts
+++ src/backend/services/ProjectService.ts
@@ -7,189 +7,176 @@ import parseLink from "parse-link-header";
 const octokit = new Octokit({ auth: config.GITHUB_ACCESS_TOKEN });
 
 const fetchProjects = async () => {
-    const projects = await prisma.project.findMany();
 
-    await Promise.all(projects.map(project => {
-        return (async () => {
-            await fetchProjectMetaData(project);
-            await fetchProjectCommits(project);
-            await fetchProjectLanguages(project);
-        })();
-    }));
+	const projects = await prisma.project.findMany();
+
+	await Promise.all(projects.map(async project => {
+		return Promise.all([updateProjectMetaData(project), upsertProjectCommits(project), upsertProjectLanguages(project)])
+	}));
 }
 
-const fetchProjectMetaData = async (project: Project) => {
+const updateProjectMetaData = async (project: Project) => {
 
-    const response = await octokit.rest.repos.get({
-        owner: config.GITHUB_REPO_OWNER,
-        repo: project.name
-    });
+	const response = await octokit.rest.repos.get({
+		owner: config.GITHUB_REPO_OWNER,
+		repo: project.name
+	});
 
-    const githubRepo = response.data;
+	const githubRepo = response.data;
 
-    if (!githubRepo) {
-        await prisma.project.delete({
-            where: {
-                id: project.id
-            }
-        })
-    }
+	if (!githubRepo) {
+		await prisma.project.delete({
+			where: {
+				id: project.id
+			}
+		})
+	}
 
-    const tags = githubRepo.topics;
+	const tags = githubRepo.topics || [];
 
-    if (!tags) {
-        await prisma.projectTag.deleteMany({
-            where: {
-                projectId: project.id
-            }
-        })
-    }
-    else {
-        await fetchProjectTags(project, tags);
-    }
+	await upsertProjectTags(project, tags);
 
-    return prisma.project.update({
-        where: {
-            id: project.id
-        },
-        data: {
-            name: githubRepo.name,
-            githubUrl: githubRepo.html_url,
-            url: githubRepo.homepage,
-            description: githubRepo.description,
-            visibility: githubRepo.visibility
-        }
-    });
+	return prisma.project.update({
+		where: {
+			id: project.id
+		},
+		data: {
+			name: githubRepo.name,
+			githubUrl: githubRepo.html_url,
+			url: githubRepo.homepage,
+			description: githubRepo.description,
+			visibility: githubRepo.visibility
+		}
+	});
 }
 
-const fetchProjectCommits = async (project: Project) => {
+const upsertProjectCommits = async (project: Project) => {
 
-    const response = await octokit.rest.repos.listCommits({
-        owner: config.GITHUB_REPO_OWNER,
-        repo: project.name,
-        per_page: 100
-    });
+	const response = await octokit.rest.repos.listCommits({
+		owner: config.GITHUB_REPO_OWNER,
+		repo: project.name,
+		per_page: 100
+	});
 
-    let commits = response.data;
+	let commits = response.data;
 
-    // concatenate bc max of per_page is 100
-    if (response.headers.link) {
+	// concatenate bc max of per_page is 100
+	if (response.headers.link) {
 
-        const parsedLink = parseLink(response.headers.link)!;
+		const parsedLink = parseLink(response.headers.link)!;
 
-        await Promise.all(new Array(Number(parsedLink.last.page) - 1).fill("").map((item, index) => {
-            return (async () => {
-                const response = await octokit.rest.repos.listCommits({
-                    owner: config.GITHUB_REPO_OWNER,
-                    repo: project.name,
-                    per_page: 100,
-                    page: index + 2
-                });
+		await Promise.all(new Array(Number(parsedLink.last.page) - 1).fill("").map(async (item, index) => {
+			const response = await octokit.rest.repos.listCommits({
+				owner: config.GITHUB_REPO_OWNER,
+				repo: project.name,
+				per_page: 100,
+				page: index + 2
+			});
 
-                commits = commits.concat(response.data);
-            })();
-        }));
-    }
+			commits = commits.concat(response.data);
+		}));
+	}
 
-    await Promise.all(commits.map(commit => {
-        return prisma.projectCommit.upsert({
-            where: {
-                id_projectId: {
-                    id: commit.node_id,
-                    projectId: project.id
-                }
-            },
-            create: {
-                id: commit.node_id,
-                projectId: project.id,
-                authorName: commit.commit.author?.name,
-                createDate: commit.commit.author?.date
-            },
-            update: {
-                authorName: commit.commit.author?.name,
-                createDate: commit.commit.author?.date
-            }
-        });
-    }));
+	await Promise.all(commits.map(commit => {
+		return prisma.projectCommit.upsert({
+			where: {
+				id_projectId: {
+					id: commit.node_id,
+					projectId: project.id
+				}
+			},
+			create: {
+				id: commit.node_id,
+				projectId: project.id,
+				authorName: commit.commit.author?.name,
+				createDate: commit.commit.author?.date
+			},
+			update: {
+				authorName: commit.commit.author?.name,
+				createDate: commit.commit.author?.date
+			}
+		});
+	}));
 }
 
-const fetchProjectLanguages = async (project: Project) => {
+const upsertProjectLanguages = async (project: Project) => {
 
-    const response = await octokit.rest.repos.listLanguages({
-        owner: config.GITHUB_REPO_OWNER,
-        repo: project.name
-    });
+	const response = await octokit.rest.repos.listLanguages({
+		owner: config.GITHUB_REPO_OWNER,
+		repo: project.name
+	});
 
-    const languages = response.data;
+	const languages = response.data;
 
-    await Promise.all(Object.keys(languages).map(languageName => {
-        return prisma.projectLanguage.upsert({
-            where: {
-                projectId_name: {
-                    projectId: project.id,
-                    name: languageName
-                }
-            },
-            create: {
-                name: languageName,
-                projectId: project.id,
-                codeAmountInBytes: languages[languageName]
-            },
-            update: {
-                name: languageName,
-                codeAmountInBytes: languages[languageName]
-            }
-        });
-    }));
+	await Promise.all(Object.keys(languages).map(languageName => {
+		return prisma.projectLanguage.upsert({
+			where: {
+				projectId_name: {
+					projectId: project.id,
+					name: languageName
+				}
+			},
+			create: {
+				name: languageName,
+				projectId: project.id,
+				codeAmountInBytes: languages[languageName]
+			},
+			update: {
+				name: languageName,
+				codeAmountInBytes: languages[languageName]
+			}
+		});
+	}));
 
-    await prisma.projectLanguage.deleteMany({
-        where: {
-            projectId: project.id,
-            name: {
-                notIn: Object.keys(languages)
-            }
-        }
-    });
+	return prisma.projectLanguage.deleteMany({
+		where: {
+			projectId: project.id,
+			name: {
+				notIn: Object.keys(languages)
+			}
+		}
+	});
+
 }
 
-const fetchProjectTags = async (project: Project, githubTags: string[]) => {
+const upsertProjectTags = async (project: Project, githubTags: string[]) => {
 
-    await Promise.all(githubTags.map(tag => {
-        return prisma.projectTag.upsert({
-            where: {
-                projectId_name: {
-                    projectId: project.id,
-                    name: tag
-                }
-            },
-            create: {
-                name: tag,
-                projectId: project.id
-            },
-            update: {
-                name: tag,
-                projectId: project.id
-            }
-        });
-    }));
+	await Promise.all(githubTags.map(tag => {
+		return prisma.projectTag.upsert({
+			where: {
+				projectId_name: {
+					projectId: project.id,
+					name: tag
+				}
+			},
+			create: {
+				name: tag,
+				projectId: project.id
+			},
+			update: {
+				name: tag,
+				projectId: project.id
+			}
+		});
+	}));
 
-    await prisma.projectTag.deleteMany({
-        where: {
-            projectId: project.id,
-            name: {
-                notIn: githubTags
-            }
-        }
-    });
+	await prisma.projectTag.deleteMany({
+		where: {
+			projectId: project.id,
+			name: {
+				notIn: githubTags
+			}
+		}
+	});
 
 }
 
 const ProjectService = {
-    fetchProjects,
-    fetchProjectMetaData,
-    fetchProjectCommits,
-    fetchProjectLanguages,
-    fetchProjectTags
+	fetchProjects,
+	updateProjectMetaData,
+	upsertProjectCommits,
+	upsertProjectLanguages,
+	upsertProjectTags
 }
 
 export default ProjectService;
