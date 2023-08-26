048dbf1895f710691283de766d4b2d0f5f56f4fa
Dyvd4 <david.kimmich@gmx.net>
2023-08-26 12:41:21 +0200

    added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import IconButton from "@components/IconButton";
 import Tooltip from "@components/Tooltip";
 import { toggleDarkMode } from "@utils/DarkModeUtils";
+import Image from "next/image";
 import Link from "next/link";
 import { FaAdjust, FaHome } from "react-icons/fa";
 import { HiFire } from "react-icons/hi";
@@ -11,48 +12,57 @@ type NavbarProps = {}
 function Navbar(props: NavbarProps) {
 	return (
 		<nav className="pl-4 pt-4 pr-4 pb-8 sticky top-0 z-50">
-			<ul className="flex justify-end items-center gap-2">
+			<ul className="flex justify-between items-center gap-2">
 				<li>
-					<Tooltip
-						direction="down"
-						title="Dark mode">
-						<IconButton onClick={toggleDarkMode}>
-							<FaAdjust />
-						</IconButton>
-					</Tooltip>
+					<Link href={"/"}>
+						<Image src={"/Logo.png"} alt="Logo" width={50} height={50} />
+					</Link>
 				</li>
 				<li>
-					<Tooltip
-						direction="down"
-						title="My projects">
-						<Link href={"/project"}>
-							<IconButton>
-								<HiFire />
-							</IconButton>
-						</Link>
-					</Tooltip>
-				</li>
-				<li>
-					<Tooltip
-						direction="down"
-						title="About me">
-						<Link href={"/about-me"}>
-							<IconButton>
-								<SiAboutdotme />
-							</IconButton>
-						</Link>
-					</Tooltip>
-				</li>
-				<li>
-					<Tooltip
-						direction="down"
-						title="Home">
-						<Link href={"/"}>
-							<IconButton>
-								<FaHome />
-							</IconButton>
-						</Link>
-					</Tooltip>
+					<ul className="flex items-center gap-2">
+						<li>
+							<Tooltip
+								direction="down"
+								title="Dark mode">
+								<IconButton onClick={toggleDarkMode}>
+									<FaAdjust />
+								</IconButton>
+							</Tooltip>
+						</li>
+						<li>
+							<Tooltip
+								direction="down"
+								title="My projects">
+								<Link href={"/project"}>
+									<IconButton>
+										<HiFire />
+									</IconButton>
+								</Link>
+							</Tooltip>
+						</li>
+						<li>
+							<Tooltip
+								direction="down"
+								title="About me">
+								<Link href={"/about-me"}>
+									<IconButton>
+										<SiAboutdotme />
+									</IconButton>
+								</Link>
+							</Tooltip>
+						</li>
+						<li>
+							<Tooltip
+								direction="down"
+								title="Home">
+								<Link href={"/"}>
+									<IconButton>
+										<FaHome />
+									</IconButton>
+								</Link>
+							</Tooltip>
+						</li>
+					</ul>
 				</li>
 			</ul>
 		</nav>
