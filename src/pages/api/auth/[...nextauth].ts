dc294718d186431053fad188a472609cab961358
Dyvd4 <david.kimmich@gmx.net>
2023-08-26 10:40:03 +0200

    added google provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,18 @@
 import { prisma } from "@prisma";
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import config from "@config/config";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: config.GOOGLE_CLIENT_ID!,
+      clientSecret: config.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: config.NEXTAUTH_SECRET!,
 };
 
 export default NextAuth(authOptions);
