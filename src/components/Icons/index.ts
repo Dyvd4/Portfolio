c0671cfdc45213088c663d593c1bd97be9a8541b
Dyvd4 <david.kimmich@gmx.net>
2023-08-27 12:12:25 +0200

    Update index.ts

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,4 +1,4 @@
-export { default as Mail } from "./SunLight";
+export { default as Mail } from "./Mail";
 export { default as LinkedIn } from "./LinkedIn";
 export { default as HalfMoon } from "./HalfMoon";
 export { default as SunLight } from "./SunLight";
