4c29172943c5611d8a67d3282ed87ceebf22b901
Dyvd4 <david.kimmich@gmx.net>
2023-09-01 23:58:55 +0200

    Update LoadingPortalSlot.tsx

--- src/components/Slots/LoadingPortalSlot.tsx
+++ src/components/Slots/LoadingPortalSlot.tsx
@@ -10,7 +10,7 @@ function LoadingPortalSlot() {
     }, []);
 
     return (
-        <div className="absolute top-1/2 left-1/2
+        <div className="absolute top-1/2 left-1/2 z-[51]
                         transform -translate-x-1/2 -translate-y-1/2"
             id="loading-portal">
         </div>
