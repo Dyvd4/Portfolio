48d3e56fcd81410f1f2f4196991b5da63324cc8c
Dyvd4 <david.kimmich@gmx.net>
2023-09-02 15:08:58 +0200

    Update index.tsx

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -3,9 +3,10 @@ import FormControl from '@components/FormControl';
 import Input from '@components/Input';
 import Modal, { ModalBody, ModalFooter, ModalHeader } from '@components/Modal';
 import useModalDisclosure from '@components/Modal/hooks/useModalDisclosure';
+import EditProjectModal from '@components/Modals/Project/EditProjectModal';
 import Textarea from '@components/Textarea';
 import { cn } from '@utils/component-utils';
-import { ComponentPropsWithRef, PropsWithChildren } from 'react';
+import { ComponentPropsWithRef, PropsWithChildren, useState } from 'react';
 
 type _IndexProps = {}
 
@@ -14,12 +15,21 @@ export type IndexProps = _IndexProps &
 
 function Index({ className, children, ...props }: IndexProps) {
     const { isActive, open, close } = useModalDisclosure()
+    const { isActive: pIsActive, open: pOpen, close: pClose } = useModalDisclosure()
+    const [projectId, setProjectId] = useState<string | undefined>();
+    const handleProjectModalOpen = () => {
+        pOpen()
+        setProjectId("340071710");
+
+    }
     return (
         <div
             className={cn(``, className)}
             {...props}>
             <Button onClick={open}>open modal</Button>
-            <Modal isLoading isActive={isActive}>
+            <Button onClick={handleProjectModalOpen}>edit project</Button>
+            <EditProjectModal projectId={projectId} isActive={pIsActive} close={pClose} />
+            {/* <Modal isActive={isActive}>
                 <ModalHeader close={close}>Contact Form</ModalHeader>
                 <ModalBody>
                     <form className="flex flex-col gap-2">
@@ -48,7 +58,7 @@ function Index({ className, children, ...props }: IndexProps) {
                         Send
                     </Button>
                 </ModalFooter>
-            </Modal>
+            </Modal> */}
         </div>
     );
 }
