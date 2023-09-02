b56c58b3034fd7c0ce9ff90ea6ff32d8abaa1f9f
Dyvd4 <david.kimmich@gmx.net>
2023-09-02 16:47:28 +0200

    Update index.ts

--- src/pages/api/projects/import/index.ts
+++ src/pages/api/projects/import/index.ts
@@ -4,16 +4,24 @@ import config from "@config/config";
 
 const { IMPORT_PROJECTS_CRON_ACCESS_TOKEN } = config;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST =
+    req.headers.authorization?.split("Bearer ")[1];
+  console.log(
+    "🚀 ~ file: index.ts:10 ~ handler ~ IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST:",
+    IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST
+  );
 
-	const IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST = req.headers.authorization?.split("Bearer ")[1];
-
-	if (IMPORT_PROJECTS_CRON_ACCESS_TOKEN !== IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST) {
-		res.status(401).json("Not authenticated to trigger import route")
-	}
-	else {
-		await ProjectService.fetchProjects();
-		res.json("Successfully imported repos from GitHub");
-	}
-
+  if (
+    IMPORT_PROJECTS_CRON_ACCESS_TOKEN !==
+    IMPORT_PROJECTS_CRON_ACCESS_TOKEN_FROM_REQUEST
+  ) {
+    res.status(401).json("Not authenticated to trigger import route");
+  } else {
+    await ProjectService.fetchProjects();
+    res.json("Successfully imported repos from GitHub");
+  }
 }
