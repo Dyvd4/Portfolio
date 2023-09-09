aa7394cbded2a2c91cbc9aaf95e8c2c796013b0d
Dyvd4 <david.kimmich@gmx.net>
2023-09-09 15:51:39 +0200

    fixed getLatestProjects

--- src/pages/api/projects/index.ts
+++ src/pages/api/projects/index.ts
@@ -1,5 +1,74 @@
 import { prisma } from "@prisma";
+import dayjs from "dayjs";
 import { NextApiRequest, NextApiResponse } from "next";
+
+export const getLatestProjects = async (projectOrTagname?: string) => {
+	const projectIds = (
+		await prisma.project.findMany({
+			select: {
+				id: true,
+			},
+			where: {
+				OR: projectOrTagname
+					? [
+							{
+								name: {
+									startsWith: projectOrTagname,
+								},
+							},
+							{
+								alias: {
+									startsWith: projectOrTagname,
+								},
+							},
+							{
+								tags: {
+									some: {
+										name: {
+											startsWith: projectOrTagname,
+										},
+									},
+								},
+							},
+					  ]
+					: undefined,
+			},
+		})
+	).map((project) => project.id);
+
+	const latestCommits = (
+		await Promise.all(
+			projectIds.map((projectId) => {
+				return (async () => {
+					const latestCommit = await prisma.projectCommit.findFirst({
+						where: {
+							projectId,
+						},
+						select: {
+							createdAt: true,
+							project: {
+								include: {
+									tags: true,
+								},
+							},
+						},
+						orderBy: {
+							createdAt: "desc",
+						},
+					});
+					return latestCommit;
+				})();
+			})
+		)
+	)
+		.filter(Boolean)
+		.sort((a, b) => {
+			return dayjs(a!.createdAt).isBefore(b!.createdAt) ? 1 : -1;
+		});
+
+	const latestProjects = latestCommits.map((commit) => commit!.project);
+	return latestProjects;
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const projectOrTagname = req.query.projectOrTagname;
@@ -7,37 +76,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	if (projectOrTagname instanceof Array) {
 		return res.status(400).json("projectOrTagname must be a string");
 	}
-
-	const projects = await prisma.project.findMany({
-		where: {
-			OR: projectOrTagname
-				? [
-						{
-							name: {
-								startsWith: projectOrTagname,
-							},
-						},
-						{
-							alias: {
-								startsWith: projectOrTagname,
-							},
-						},
-						{
-							tags: {
-								some: {
-									name: {
-										startsWith: projectOrTagname,
-									},
-								},
-							},
-						},
-				  ]
-				: undefined,
-		},
-		include: {
-			tags: true,
-		},
-	});
+	const projects = await getLatestProjects(projectOrTagname);
 
 	res.json(projects);
 }
