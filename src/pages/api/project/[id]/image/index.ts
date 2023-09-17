263e7b63ebb1c85d819843cf475f0892c7e064c2
Dyvd4 <david.kimmich@gmx.net>
2023-09-17 15:28:21 +0200

    Update index.ts

--- src/pages/api/project/[id]/image/index.ts
+++ src/pages/api/project/[id]/image/index.ts
@@ -13,10 +13,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	});
 	if (!project) return res.status(404).json("project not found");
 	const imageDataUrl = project.imageUrl;
-	const contentType = imageDataUrl!.split("data:")[1].split(";")[0];
+	const contentType = imageDataUrl.split("data:")[1].split(";")[0];
 	res.setHeader("Content-Type", contentType);
 	res.setHeader("Content-Disposition", `inline"`);
-	const imageData = imageDataUrl!.split(",")[1];
+	const imageData = imageDataUrl.split(",")[1];
 	const imageBuffer = Buffer.from(imageData, "base64");
 	res.send(imageBuffer);
 }
