110525d5a1447e6ed943e0b5ad6756415af0f098
Dyvd4 <david.kimmich@gmx.net>
2023-09-17 13:51:44 +0200

    Update useStaticImageUrl.ts

--- src/hooks/useStaticImageUrl.ts
+++ src/hooks/useStaticImageUrl.ts
@@ -4,6 +4,6 @@ import { StaticImageData } from "next/image";
 const { NEXT_PUBLIC_BASE_URL } = config;
 
 const useStaticImageUrl = (image: StaticImageData) => {
-	return `${NEXT_PUBLIC_BASE_URL}/${image.src}`;
+	return `${NEXT_PUBLIC_BASE_URL}${image.src}`;
 };
 export default useStaticImageUrl;
