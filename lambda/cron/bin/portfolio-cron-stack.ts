c7c6d9ac4f0119b4823c24bcefc0185cf500ac68
Dyvd4 <david.kimmich@gmx.net>
2024-02-14 15:07:37 +0100

    removed lambda repo

--- lambda/cron/bin/portfolio-cron-stack.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { PortfolioCronStack } from "../lib/portfolio-cron-stack";
-
-const app = new cdk.App();
-new PortfolioCronStack(app, "PortfolioCronStack");
--- sst.config.ts
+++ sst.config.ts
@@ -4,15 +4,13 @@ import { NextjsSite } from "sst/constructs";
 export default {
 	config(_input) {
 		return {
-			name: "Portfolio",
-			region: "eu-central-1",
+			name: "portfolio",
+			region: "us-east-1",
 		};
 	},
 	stacks(app) {
 		app.stack(function Site({ stack }) {
-			const site = new NextjsSite(stack, "site", {
-				memorySize: "2048 MB"
-			});
+			const site = new NextjsSite(stack, "site");
 
 			stack.addOutputs({
 				SiteUrl: site.url,
