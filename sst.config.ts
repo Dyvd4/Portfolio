e1daae139dad93fed4be18ed64cb20da67834d48
Dyvd4 <david.kimmich@gmx.net>
2024-02-14 13:32:53 +0100

    Update sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -2,19 +2,21 @@ import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
 export default {
-  config(_input) {
-    return {
-      name: "Portfolio",
-      region: "eu-central-1",
-    };
-  },
-  stacks(app) {
-    app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+	config(_input) {
+		return {
+			name: "Portfolio",
+			region: "eu-central-1",
+		};
+	},
+	stacks(app) {
+		app.stack(function Site({ stack }) {
+			const site = new NextjsSite(stack, "site", {
+				memorySize: "2048 MB"
+			});
 
-      stack.addOutputs({
-        SiteUrl: site.url,
-      });
-    });
-  },
+			stack.addOutputs({
+				SiteUrl: site.url,
+			});
+		});
+	},
 } satisfies SSTConfig;
