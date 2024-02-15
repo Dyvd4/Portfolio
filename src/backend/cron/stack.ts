a766b46c4e74cf98caa05717d3275fb69b4bbad4
Dyvd4 <david.kimmich@gmx.net>
2024-02-15 18:57:27 +0100

    Update stack.ts

--- src/backend/cron/stack.ts
+++ src/backend/cron/stack.ts
@@ -16,7 +16,7 @@ export function CronStack({ stack }: StackContext) {
 		schedule: scheduleRate,
 		job: {
 			function: {
-				handler: "import-repos.handler",
+				handler: "src/backend/cron/import-repos.handler",
 				environment
 			}
 		},
@@ -26,7 +26,7 @@ export function CronStack({ stack }: StackContext) {
 		schedule: scheduleRate,
 		job: {
 			function: {
-				handler: "import-projects.handler",
+				handler: "src/backend/cron/import-projects.handler",
 				environment
 			}
 		},
