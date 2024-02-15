61f0004799534c17f9551088111a22f7da642ee6
Dyvd4 <david.kimmich@gmx.net>
2024-02-15 15:07:40 +0100

    fixed environment variables

--- src/backend/cron/stack.ts
+++ src/backend/cron/stack.ts
@@ -2,12 +2,33 @@ import { Cron, StackContext } from "sst/constructs";
 
 export function CronStack({ stack }: StackContext) {
 	const scheduleRate = "rate(1 day)"
+
+	// env-variables need to be explicitly passed here, as the handlers
+	// get deployed to separate lambda functions, which have their own env-variables each
+	const environment = {
+		GH_REPO_OWNER: process.env.GH_REPO_OWNER!,
+		GH_API_URL: process.env.GH_API_URL!,
+		GH_ACCESS_TOKEN: process.env.GH_ACCESS_TOKEN!,
+		DATABASE_URL: process.env.DATABASE_URL!
+	}
+
 	new Cron(stack, "Import-repos", {
 		schedule: scheduleRate,
-		job: "./import-repos.ts",
+		job: {
+			function: {
+				handler: "./import-repos.ts",
+				environment
+			}
+		},
 	});
+
 	new Cron(stack, "Import-projects", {
 		schedule: scheduleRate,
-		job: "./import-projects.ts",
+		job: {
+			function: {
+				handler: "./import-projects.ts",
+				environment
+			}
+		},
 	});
 }
