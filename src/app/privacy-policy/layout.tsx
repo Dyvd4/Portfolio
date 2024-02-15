1f09a1cacb33e42f09e9061e323e002d694699e3
Dyvd4 <david.kimmich@gmx.net>
2024-02-15 21:14:28 +0100

    remove og from metadata

--- src/app/privacy-policy/layout.tsx
+++ src/app/privacy-policy/layout.tsx
@@ -1,18 +1,8 @@
 import { PAGE_TITLE } from "@app/privacy-policy";
 import { Metadata } from "next/types";
-import { getOgImageUrl } from "@utils/utils";
-
-const ogImageTitle = "privacy_policy";
-const ogImage = { url: getOgImageUrl(ogImageTitle), alt: ogImageTitle };
 
 export const metadata: Metadata = {
 	title: PAGE_TITLE,
-	openGraph: {
-		images: ogImage,
-	},
-	twitter: {
-		images: ogImage,
-	},
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
