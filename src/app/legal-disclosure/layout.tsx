cc4a33da70581f7cd25d1a2dea562772fb1b53b6
Dyvd4 <david.kimmich@gmx.net>
2024-02-16 12:23:23 +0100

    remove file-based open-graph images and import them statically
    -- somehow, SST doen't deploy the file-based og images properly

--- src/app/legal-disclosure/layout.tsx
+++ src/app/legal-disclosure/layout.tsx
@@ -1,8 +1,21 @@
 import { PAGE_TITLE } from "@app/legal-disclosure";
 import { Metadata } from "next/types";
+import OgImage from "@public/og/legal-disclosure.png"
 
 export const metadata: Metadata = {
 	title: PAGE_TITLE,
+	openGraph: {
+		images: [{
+			url: OgImage.src,
+			alt: "legal disclosure"
+		}]
+	},
+	twitter: {
+		images: [{
+			url: OgImage.src,
+			alt: "legal disclosure"
+		}]
+	}
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
--- src/app/privacy-policy/layout.tsx
+++ src/app/privacy-policy/layout.tsx
@@ -1,8 +1,21 @@
 import { PAGE_TITLE } from "@app/privacy-policy";
 import { Metadata } from "next/types";
+import OgImage from "@public/og/privacy-policy.png"
 
 export const metadata: Metadata = {
 	title: PAGE_TITLE,
+	openGraph: {
+		images: [{
+			url: OgImage.src,
+			alt: "privacy policy"
+		}]
+	},
+	twitter: {
+		images: [{
+			url: OgImage.src,
+			alt: "privacy policy"
+		}]
+	}
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
