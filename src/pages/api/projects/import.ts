5edd112e6ead0334494becdf3efd7212a278f5a0
Dyvd4 <david.kimmich@gmx.net>
2024-05-26 09:19:47 +0200

    Update import.ts

--- src/pages/api/projects/import.ts
+++ src/pages/api/projects/import.ts
@@ -3,18 +3,19 @@ import { isAuthenticated } from "@backend/utils/auth-utils";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	console.log(process.env.PORTFOLIO_DATABASE_URL);
+
 	if (!(await isAuthenticated(req, res))) {
 		return res.status(401).json("Not authenticated to access this route");
 	}
 
 	try {
 		await ProjectService.fetchProjects();
-	}
-	catch (e) {
-		console.error(e)
+	} catch (e) {
+		console.error(e);
 		return res.status(500).json("Internal server error");
 	}
-	const responseMsg = "Successfully updated projects with repo data from GitHub"
-	console.log(responseMsg)
+	const responseMsg = "Successfully updated projects with repo data from GitHub";
+	console.log(responseMsg);
 	return res.json(responseMsg);
 }
