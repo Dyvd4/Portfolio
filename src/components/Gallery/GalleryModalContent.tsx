ec52321e9ae53d2bddec400f1c1d51f238bb7592
Dyvd4 <dueftt@gmail.com>
2024-08-10 16:01:22 +0200

    Update GalleryModalContent.tsx

--- src/components/Gallery/GalleryModalContent.tsx
+++ src/components/Gallery/GalleryModalContent.tsx
@@ -93,7 +93,7 @@ export default function GalleryModalContent({
 				{...handlers}
 			>
 				{/* Main image */}
-				<div className="w-full overflow-hidden">
+				<div className="w-full">
 					<div className="relative flex aspect-[3/2] items-center justify-center">
 						<AnimatePresence initial={false} custom={direction}>
 							<motion.div
@@ -116,118 +116,115 @@ export default function GalleryModalContent({
 								/>
 							</motion.div>
 						</AnimatePresence>
+
+						{loaded && (
+							<>
+								{curIndex > 0 && (
+									<button
+										className="absolute left-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
+										style={{ transform: "translate3d(0, 0, 0)" }}
+										onClick={() => changeCurrentIndex(curIndex - 1)}
+									>
+										<ChevronLeftIcon className="h-6 w-6" />
+									</button>
+								)}
+								{curIndex + 1 < images.length && (
+									<button
+										className="absolute right-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
+										style={{ transform: "translate3d(0, 0, 0)" }}
+										onClick={() => changeCurrentIndex(curIndex + 1)}
+									>
+										<ChevronRightIcon className="h-6 w-6" />
+									</button>
+								)}
+								<div className="absolute left-0 top-0 z-20 flex -translate-y-full transform items-center gap-2 p-3 text-white">
+									{/* full size version */}
+									<a
+										href={curren_TImage.src}
+										className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+										target="_blank"
+										title="Open fullsize version"
+										rel="noreferrer"
+									>
+										<ExternalLink className="h-5 w-5" />
+									</a>
+									{/* download */}
+									<button
+										onClick={() =>
+											downloadImage(curren_TImage.src, `${curIndex}.jpg`)
+										}
+										className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+										title="Download fullsize version"
+									>
+										<Download className="h-5 w-5" />
+									</button>
+								</div>{" "}
+								<div className="absolute right-0 top-0 flex -translate-y-full transform items-center gap-2 p-3 text-white">
+									<button
+										onClick={() => closeModal()}
+										className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+									>
+										<XCircle className="h-5 w-5" />
+									</button>
+								</div>
+							</>
+						)}
 					</div>
 				</div>
 
-				{/* Buttons + bottom nav bar */}
-				<div className="absolute inset-0 mx-auto flex max-w-7xl items-center justify-center">
-					{/* Buttons */}
-					{loaded && (
-						<div className="relative aspect-[3/2] max-h-full w-full">
-							{curIndex > 0 && (
-								<button
-									className="absolute left-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
-									style={{ transform: "translate3d(0, 0, 0)" }}
-									onClick={() => changeCurrentIndex(curIndex - 1)}
+				{/* Bottom Nav bar */}
+				<div className="fixed inset-x-0 bottom-0 z-40 overflow-hidden bg-gradient-to-b from-black/0 to-black/60">
+					<motion.div
+						initial={false}
+						className="mx-auto mb-6 mt-6 flex aspect-[3/2] h-14"
+					>
+						<AnimatePresence initial={false}>
+							{filteredImages.map(({ src, id }, i) => (
+								<motion.button
+									initial={{
+										width: "0%",
+										x: `${Math.max((curIndex - 1) * -100, 15 * -100)}%`,
+									}}
+									animate={{
+										scale: i === curIndex ? 1.25 : 1,
+										width: "100%",
+										x: `${Math.max(curIndex * -100, 15 * -100)}%`,
+									}}
+									exit={{ width: "0%" }}
+									onClick={() => changeCurrentIndex(i)}
+									key={i}
+									className={`${
+										i === curIndex
+											? "z-20 rounded-md shadow shadow-black/50"
+											: "z-10"
+									} ${i === 0 ? "rounded-l-md" : ""} ${
+										i === images.length - 1 ? "rounded-r-md" : ""
+									} relative inline-block w-full shrink-0 transform-gpu overflow-hidden focus:outline-none`}
 								>
-									<ChevronLeftIcon className="h-6 w-6" />
-								</button>
-							)}
-							{curIndex + 1 < images.length && (
-								<button
-									className="absolute right-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
-									style={{ transform: "translate3d(0, 0, 0)" }}
-									onClick={() => changeCurrentIndex(curIndex + 1)}
-								>
-									<ChevronRightIcon className="h-6 w-6" />
-								</button>
-							)}
-							<div className="absolute left-0 top-0 flex items-center gap-2 p-3 text-white">
-								{/* full size version */}
-								<a
-									href={curren_TImage.src}
-									className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
-									target="_blank"
-									title="Open fullsize version"
-									rel="noreferrer"
-								>
-									<ExternalLink className="h-5 w-5" />
-								</a>
-								{/* download */}
-								<button
-									onClick={() =>
-										downloadImage(curren_TImage.src, `${curIndex}.jpg`)
-									}
-									className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
-									title="Download fullsize version"
-								>
-									<Download className="h-5 w-5" />
-								</button>
-							</div>
-							<div className="absolute right-0 top-0 flex items-center gap-2 p-3 text-white">
-								<button
-									onClick={() => closeModal()}
-									className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
-								>
-									<XCircle className="h-5 w-5" />
-								</button>
-							</div>
-						</div>
-					)}
-					{/* Bottom Nav bar */}
-					<div className="fixed inset-x-0 bottom-0 z-40 overflow-hidden bg-gradient-to-b from-black/0 to-black/60">
-						<motion.div
-							initial={false}
-							className="mx-auto mb-6 mt-6 flex aspect-[3/2] h-14"
-						>
-							<AnimatePresence initial={false}>
-								{filteredImages.map(({ src, id }, i) => (
-									<motion.button
-										initial={{
-											width: "0%",
-											x: `${Math.max((curIndex - 1) * -100, 15 * -100)}%`,
+									<Link
+										href={{
+											query: {
+												imageId: id,
+											},
 										}}
-										animate={{
-											scale: i === curIndex ? 1.25 : 1,
-											width: "100%",
-											x: `${Math.max(curIndex * -100, 15 * -100)}%`,
-										}}
-										exit={{ width: "0%" }}
-										onClick={() => changeCurrentIndex(i)}
-										key={i}
-										className={`${
-											i === curIndex
-												? "z-20 rounded-md shadow shadow-black/50"
-												: "z-10"
-										} ${i === 0 ? "rounded-l-md" : ""} ${
-											i === images.length - 1 ? "rounded-r-md" : ""
-										} relative inline-block w-full shrink-0 transform-gpu overflow-hidden focus:outline-none`}
+										scroll={false}
 									>
-										<Link
-											href={{
-												query: {
-													imageId: id,
-												},
-											}}
-											scroll={false}
-										>
-											<Image
-												alt="small photos on the bottom"
-												width={180}
-												height={120}
-												className={`${
-													i === curIndex
-														? "brightness-110 hover:brightness-110"
-														: "brightness-50 contrast-125 hover:brightness-75"
-												} h-full transform object-cover transition`}
-												src={src}
-											/>
-										</Link>
-									</motion.button>
-								))}
-							</AnimatePresence>
-						</motion.div>
-					</div>
+										<Image
+											alt="small photos on the bottom"
+											width={180}
+											height={120}
+											className={`${
+												i === curIndex
+													? "brightness-110 hover:brightness-110"
+													: "brightness-50 contrast-125 hover:brightness-75"
+											} h-full transform object-cover transition`}
+											src={src}
+										/>
+									</Link>
+								</motion.button>
+							))}
+						</AnimatePresence>
+					</motion.div>
 				</div>
 			</div>
 		</MotionConfig>
