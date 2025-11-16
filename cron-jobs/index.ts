15c6aac19231c983075f5cc90fff9f621ed4a24d
dyvd <david@kimmich.dev>
2025-11-16 16:14:39 +0100

    test

--- cron-jobs/index.ts
+++ cron-jobs/index.ts
@@ -2,10 +2,10 @@ import { handler as importProjects } from "@/jobs/import-projects";
 import { handler as importRepos } from "@/jobs/import-repos";
 import cron from "node-cron";
 
-cron.schedule("0 0 * * *", () => {
+cron.schedule("* * * * *", () => {
 	importProjects();
 });
 
-cron.schedule("0 0 * * *", () => {
+cron.schedule("* * * * *", () => {
 	importRepos();
 });
