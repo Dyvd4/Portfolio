d5ef144ce76c5719034b4b2f56e8b00bd662cde6
dyvd <david@kimmich.dev>
2025-11-23 16:12:49 +0100

    add proper error handling to auth-utils

--- src/backend/utils/auth-utils.ts
+++ src/backend/utils/auth-utils.ts
@@ -1,12 +1,23 @@
 import config from "@config/config";
 import { authOptions } from "@pages/api/auth/[...nextauth]";
+import jwt from "jsonwebtoken";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
-import jwt from "jsonwebtoken";
+import logger from "../../lib/logger";
 
 export const isAuthenticated = async (req: NextApiRequest, res: NextApiResponse) => {
 	const authToken = req.headers["auth-token"]! as string;
-	if (!authToken) return false;
+	if (authToken == null) return false;
+
 	const session = await getServerSession(req, res, authOptions);
-	return !!session || !!jwt.verify(authToken, config.NEXTAUTH_SECRET);
+
+	let jwtSession: any;
+	try {
+		jwtSession = jwt.verify(authToken, config.NEXTAUTH_SECRET);
+	} catch (err) {
+		logger.error(err);
+		return false;
+	}
+
+	return session != null || jwtSession != null;
 };
